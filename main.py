8cd46d75380f5d5c09cb8246df3651bdc31738f8
Vaibhav Mahindru <vaibhav.mahindru@plaksha.edu.in>
2025-04-07 23:01:10 +0530

    pushing the secret file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 # deepseek key
-deepseekApiKey = st.secrets["deepseek"]["api_key"]
+# deepseekApiKey = st.secrets["deepseek"]["api_key"]
 
 geminiApiKey = st.secrets["gemini"]["geminiApiKey"]
 # Page config
